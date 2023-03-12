60f714afdb121f6bc5493e5cfc6bf4c0bf61f608
sania <saniaamanati@student.uns.ac.id>
2023-03-12 11:47:55 +0700

    commit kedua

---
+++ I0322115_Modul-01.py
@@ -0,0 +1,11 @@
+Nama = "Sania Amanati"
+NIM = "I0322115"
+Tempat_tinggal = "Desa Sroyo, Kecamatan Jaten, Kabupaten Karanganyar"
+Visualisasi = "Desa Sroyo termasuk wilayah di kabupaten Karanganyar yang letaknya berada di pinggir kota Solo, sehingga untuk menempuh ke Universitas Sebelas Maret kurang lebih hanya memerlukan waktu 15 menit."
+Harapan = "Harapan saya setelah lulus dari Teknik Industri adalah ilmu yang saya peroleh dapat diterapkan didunia kerja, baik hardskill maupun softskill."
+
+print(Nama)
+print(NIM)
+print(Tempat_tinggal)
+print(Visualisasi)
+print(Harapan)
